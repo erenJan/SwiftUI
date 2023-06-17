c2fa78774f2634705739d27afe8f83463cd9724a
Erencan Erel <erencanerel@gmail.com>
2023-06-17 07:13:20 +0300

    xcode init

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Erencan Erel on 17.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
