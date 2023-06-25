3b08dc218546640f4c7365dc4240c9a6c45399e6
Erencan Erel <erencanerel@gmail.com>
2023-06-25 19:48:51 +0300

    navigationList

--- ToDoApp/ToDoApp/ContentView.swift
+++ ToDoApp/ToDoApp/ContentView.swift
@@ -15,42 +15,112 @@ struct CheckListItem : Hashable{
 }
 
 struct ContentView: View {
-    @State private var searchText = ""
-    var shoppingList : [CheckListItem] = [
+    @State private var editMode = EditMode.inactive
+    @State private var showingSheet = false
+    @State var shoppingList : [CheckListItem] = [
         CheckListItem(isChecked: false, title: "Egg",description: "happy chicken egg otherwise do not buy!"),
         CheckListItem(isChecked: false, title: "Milk",description: "if there is no egg, do not buy!"),
         CheckListItem(isChecked: false, title: "Bread"),
         CheckListItem(isChecked: false, title: "Ice Tea"),
-        CheckListItem(isChecked: false, title: "New Macbook",description: "ask your mac's price first!")
+        CheckListItem(isChecked: true, title: "New Macbook",description: "ask your mac's price first!")
     ]
     
     
     var body: some View {
         NavigationView{
-            List{
-                ForEach(searchResults, id: \.self){
+            List(){
+                ForEach(shoppingList, id: \.self){
                     msg in
                     VStack(alignment: .leading){
-                        Text(msg.title).font(.system(size: 22)).fontWeight(.bold)
-                        msg.description {
-                            Text(msg.description!).font(.system(size:14))
+                        HStack{
+                            if msg.isChecked {
+                                ZStack{
+                                    Circle()
+                                        .fill(Color.blue)
+                                        .frame(width: 20, height: 20)
+                                    Circle()
+                                        .fill(Color.white)
+                                        .frame(width: 8, height: 8)
+                                }
+                            } else {
+                                Circle()
+                                    .fill(Color.white)
+                                    .frame(width: 20, height: 20)
+                                    .overlay(Circle().stroke(Color.gray, lineWidth: 1))
+                            }
+                            VStack(alignment: .leading){
+                                Text(msg.title).font(.system(size: 22)).fontWeight(.bold)
+                                if let desc = msg.description {
+                                    Text(msg.description!).font(.system(size:14))
+                                }
+                            }
                         }
+                    }.gesture(TapGesture().onEnded {
+                        if let i = shoppingList.firstIndex(where: { $0.title == msg.title }) {
+                            shoppingList[i].isChecked = !shoppingList[i].isChecked
+                            }
+                        }
+                    )
+                    
+                }.onDelete(perform: onDelete)
+                    .onMove(perform: onMove)
+            }.navigationBarTitle("Shopping List").navigationBarItems(
+//                leading: HStack {
+//                Button("< Go Back") {
+//                    print("Hours tapped!")
+//                }
+//            },
+                trailing: addButton)
+        }
+    }
+    private var addButton: some View {
+        switch editMode {
+            case .inactive:
+                return AnyView(Button(action: {
+                    showingSheet.toggle()
+                }) {
+                    Image(systemName: "plus")
+                }
+                .sheet(isPresented: $showingSheet,onDismiss: didDismiss) {
+                    CustomSheet.padding(16)
+                    Spacer()
+                })
+        default:
+            return AnyView(EmptyView())
+        }
+    }
+    private var CustomSheet : some View {
+        return AnyView(
+            VStack(){
+                Rectangle().foregroundColor(Color.gray).frame(width: 100,height: 6).cornerRadius(90)
+                HStack{
+                    Text("New ToDo").padding(.leading,10).padding(.top,16).font(.system(size: 28).bold())
+                    Spacer()
+                    Text("Dismiss").foregroundColor(Color.red).onTapGesture {
+                        showingSheet = false
                     }
                 }
-            }.searchable(text: $searchText).navigationBarTitle("Shopping List")
-        }
-    }
-    var searchResults: [CheckListItem] {
-        if searchText.isEmpty{
-            return shoppingList
-        }else{
-            return shoppingList.filter{
-                $0.title.lowercased().contains(searchText.lowercased())
+                
             }
-        }
+        )
     }
+    private func onDelete(offsets: IndexSet) {
+        shoppingList.remove(atOffsets: offsets)
+    }
+    private func onMove(source: IndexSet, destination: Int) {
+        shoppingList.move(fromOffsets: source, toOffset: destination)
+    }
+    func onAdd() {
+        NavigationView {
+                Text("deneme")
+            }
+            .navigationViewStyle(StackNavigationViewStyle())
+    }
+    func didDismiss() {
+        // Handle the dismissing action.
+    }
+    
 }
-
 
 
 
@@ -61,3 +131,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
