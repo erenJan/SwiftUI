f5643be32a5de0ddb075e8f322fb8ad8d5e9408d
Erencan Erel <erencanerel@gmail.com>
2023-06-26 12:07:02 +0300

    drawer_left

--- MyApp/MyApp/SideBar.swift
+++ MyApp/MyApp/SideBar.swift
@@ -23,10 +23,11 @@ let ShoppingList = MenuListItem(name: "Shopping List", icon: "cart")
 let TodoList = MenuListItem(name: "To Do", icon: "list.bullet.clipboard")
 var bagContents = [TodoList,ShoppingList,Investing,BookLibrary,MovieLibrary]
 
+
 struct SideBar: View {
     @Binding var isSidebarVisible : Bool
     var sideBarWidth = UIScreen.main.bounds.size.width * 0.7
-    var bgColor : Color = Color(.init(red:52/255,green: 70/255,blue: 182/255,alpha:1))
+    
     var body: some View {
         ZStack{
             GeometryReader{
@@ -42,14 +43,26 @@ struct SideBar: View {
     }
     var content : some View {
         HStack(alignment: .top){
-            ZStack(alignment: .top){
-                bgColor
-                menuList
-            }
+                VStack{
+                    persona.padding(.top,40)
+                    menuList
+                    actionsPanel
+                }.background(Color(UIColor.systemBackground))
             .frame(width: sideBarWidth)
             .offset(x:isSidebarVisible ? 0 : -sideBarWidth)
             .animation(.default, value: isSidebarVisible)
             
+            Spacer()
+        }
+    }
+    var persona : some View{
+        HStack(alignment: .top){
+            Image("erencan_erel").resizable().cornerRadius(25).padding(10)
+                    .scaledToFill().frame(width: 100, height: 100)
+            VStack(alignment: .leading){
+                Text("Erencan Erel").font(.title2.bold())
+                Text("Computer Engineer").font(.system(size: 14))
+            }.padding(.top,20)
             Spacer()
         }
     }
@@ -58,9 +71,26 @@ struct SideBar: View {
             row in
             Image(systemName: row.icon)
             Text(row.name).font(.title3)
-        }.padding(.top,30)
+        }
     }
-    
+    var actionsPanel : some View{
+        HStack(alignment: .center){
+            Button(action: {}){
+                HStack{
+                    Image(systemName: "gear")
+                    Text("Settigns").font(.title3).padding(.horizontal, 8)
+                        .cornerRadius(8)
+                }
+            }.buttonStyle(.borderedProminent)
+            Button(action: {}){
+                HStack{
+                    Image(systemName: "log.off")
+                    Text("Log Out").font(.title3).padding(.horizontal, 8)
+                        .cornerRadius(8)
+                }
+            }.buttonStyle(.bordered)
+        }.padding(.bottom,40).padding(.top,20)
+    }
 }
 struct SideBar_Previews: PreviewProvider {
     
